869a4ac97a32fe6ac28a5d93b57fa72fc7439d67
splidsboel <jseg98@gmail.com>
2025-02-16 12:02:36 +0100

    hw1 færdig

--- hw1/three_ddl.sql
+++ hw1/three_ddl.sql
@@ -3,11 +3,11 @@ CREATE SEQUENCE seq_semester_id START 1;
 CREATE SEQUENCE seq_examiner_id START 1;
 CREATE SEQUENCE seq_course_id START 1;
 
+--entities
 CREATE TABLE Student (
     StudID INT PRIMARY KEY DEFAULT nextval ('seq_student_id'),
     student_name VARCHAR(30) NOT NULL,
-    email varchar(50) NOT NULL,
-    FOREIGN KEY (SemID) INT REFERENCES Semester(SemID)
+    student_email varchar(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE Semester (
@@ -15,21 +15,48 @@ CREATE TABLE Semester (
     year INT NOT NULL,
     season VARCHAR(10) NOT NULL, 
     CHECK (season IN('Autumn', 'Spring'))
-)
+);
 
 CREATE TABLE Course(
     CID INT PRIMARY KEY DEFAULT nextval ('seq_course_id'),
     course_name VARCHAR(30) NOT NULL,
     capacity INT,
     CHECK(capacity<301)
-)
+);
 
 CREATE TABLE Examiner(
-    
-)
+    EID INT PRIMARY KEY DEFAULT nextval ('seq_examiner_id'),
+    examiner_name varchar(30) not NULL,
+    examiner_email varchar(50) not NULL UNIQUE
+);
 
-drop table Student;
+---relationships
+CREATE TABLE takes(
+    StudID INT not null,
+    CID INT not null,
+    SemID INT not null,
+    room VARCHAR(100),
+    PRIMARY KEY (StudID, CID, SemID),
+    FOREIGN KEY (StudID) REFERENCES Student(StudID),
+    FOREIGN KEY (CID) REFERENCES Course(CID),
+    FOREIGN KEY (SemID) REFERENCES Semester(SemID)
+);
 
-show tables;
+CREATE TABLE grades(
+    StudID INT not null,
+    CID INT not null,
+    SemID INT not null,
+    EID INT not null,
+    grade varchar(30),
+    PRIMARY KEY (StudID, CID, SemID, EID),
+    FOREIGN KEY (StudID, CID, SemID) REFERENCES takes(StudID, CID, SemID),
+    FOREIGN KEY (EID) REFERENCES Examiner(EID)
+);
 
-pragma table_info;
+CREATE TABLE starts_in(
+    SemID INT not null,
+    StudID INT not null,
+    PRIMARY KEY (SemID, StudID),
+    FOREIGN KEY (StudID) REFERENCES Student(StudID),
+    FOREIGN KEY (SemID) REFERENCES Semester(SemID)
+);
